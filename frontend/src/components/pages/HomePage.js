0e8dd45b842e51bba18f0af295b590166bfd29d2
geraldine <hypatia.of.alexandria55@gmail.com>
2021-05-08 22:58:17 -0400

    basic info homepage

--- frontend/src/components/pages/HomePage.js
+++ frontend/src/components/pages/HomePage.js
@@ -1,23 +1,95 @@
 import React, { useRef, useState, useEffect } from 'react'
-import { Container, Typography, Button, Paper } from '@material-ui/core'
+import { Container, Typography, Button, Paper, Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom'
 import './HomePage.css'
 import * as Const from './constants.js'
 
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(5),
+    margin: 'auto',
+    maxWidth: '80%',
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    width: '50%',
+    height: '50%',
+  },
+}));
+
+
 function InfoBlock(props) {
-  return (
-    <Paper elevation={4} className="home-paper" variant="outlined">
-      <Typography>
-        <h1> {props.title}</h1>
+  const classes = useStyles();
+  const image= process.env.PUBLIC_URL + props.pic;
+  var typoBlock;
+  if(props.buttonLink==null) {
+  typoBlock = (
+    <Grid container spacing={7} direction="row">
+    
+    <Grid item xs>
+    <Typography>
+     <h1> {props.title}</h1>
+     {props.para}
+    </Typography>
+    </Grid>
+
+    <Grid item>
+      <img className={classes.image} src={image} />
+    </Grid>
+
+    </Grid>
+  )} else {
+    typoBlock = (
+      <Grid container spacing={7} direction="row">
+
+        <Grid container direction="column" justify="center" alignItems="center"item xs={12} sm>
+        <Grid item xs>
+        <Typography>
+        <h1> {props.title}</h1> <br />
         {props.para}
-      </Typography>
+        </Typography>
+        </Grid>
+        <Grid item>
+        <Link to="/test">
+          <Button variant="contained" color="secondary">
+            <Typography variant="h6" component="h6">
+            Take Test
+            </Typography>
+          </Button>
+        </Link>
+        </Grid>
+        </Grid>
+
+      <Grid item>
+        <img className={classes.image} src={image} />
+      </Grid>
+
+      </Grid>
+    )
+  }
+  return (
+    <Grid item xs={12}>
+    <Paper elevation={2} className={classes.paper}>
+      {typoBlock}
     </Paper>
+    </Grid>
   )
 }
 
 
 function HomePage() {
   const middleRef = useRef()
+  const classes = useStyles();
 
   const [showArrow, setShowArrow] = useState(true)
   const onArrowClick = (event) => {
@@ -56,10 +128,13 @@ function HomePage() {
           )
         }
       </Container>
-      <Container className="main-container middle-container" ref={middleRef}>
-        <InfoBlock title="What is Parkinsons Disease?" para={Const.PARKINSONS_INFO}></InfoBlock>
-      
-        <InfoBlock title="What is Parkinsons Test?" para={Const.PARKINSONS_INFO}></InfoBlock>
+      <Container className="main-container middle-container" >
+      <Grid className={classes.root} container spacing={4} style={{color: 'aliceblue'}}>
+        <InfoBlock title="What is Parkinsons Disease?" para={Const.PARKINSONS_INFO} pic="fall_down_orange.png"></InfoBlock>
+        <InfoBlock title="What is Parkinsons Test?" para={Const.TEST_INFO} pic="data_computer_pink.svg"></InfoBlock>
+        <div ref={middleRef}></div>
+        <InfoBlock title="Why use Parkinsons Test?" para={Const.WHY_TEST} buttonLink="" pic="cup_tea_blue.svg"></InfoBlock>
+      </Grid>
       </Container>
     </div>
   )
--- frontend/src/components/pages/constants.js
+++ frontend/src/components/pages/constants.js
@@ -4,3 +4,5 @@ export const RESOURCES = {"NIH:": "https://www.nia.nih.gov/health/parkinsons-dis
 
 // **
 export const TEST_INFO = "Using data from patients with Parkisons, the test uses .... to predict(?) Parkinsons."
+
+export const WHY_TEST = "Taking the test can warn you if you have common early symptoms of Parkinsons, allowing you to follow up with your doctor. Whie no specific tests currently exist to diagnose Parkinson's, it's important to start treatment early when possible."
